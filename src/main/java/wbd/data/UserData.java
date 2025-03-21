6a4fd63f47e589d72fb7f62e8e8eb1450d4a1ae4
Natalia Tikhomirova <ntikhomirova@gmail.com>
2025-03-21 20:56:10 +0100

    Added the 'wbd' packages under main/java and test/java, and created the 'test_logs' (and 'test_screenshots') directories in src/test

---
+++ src/main/java/wbd/data/UserData.java
@@ -0,0 +1,8 @@
+package wbd.data;
+
+public class UserData {
+    public static final String VALID_EMAIL = "";
+    public static final String VALID_PASSWORD = "";
+    public static final String INVALID_EMAIL = "invalidUser";
+    public static final String INVALID_PASSWORD = "invalidPassword";
+}
---
+++ src/main/java/wbd/utils/RetryAnalyser.java
@@ -0,0 +1,19 @@
+package wbd.utils;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyser implements IRetryAnalyzer {
+
+    private int retryCount = 0;
+    private static final int maxRetryCount = 5;
+    @Override
+    public boolean retry(ITestResult result) {
+        if (retryCount < maxRetryCount) {
+            retryCount++;
+            System.out.println("Retrying test ["+result.getName() + "] within ["+ retryCount +"] times");
+            return true;
+        }
+        return false;
+    }
+}
