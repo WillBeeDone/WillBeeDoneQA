bd82c83e1e97bdd8e4c76b63196f5a4e3b00c2ec
AnastasiiaPodlesna <nisana@ukr.net>
2025-03-31 20:51:36 +0200

    add web test registration

--- src/main/java/wbd/web/data/UserData.java
+++ src/main/java/wbd/web/data/UserData.java
@@ -1,8 +1,8 @@
 package wbd.web.data;
 
 public class UserData {
-    public static final String VALID_EMAIL = "";
-    public static final String VALID_PASSWORD = "";
+    public static final String VALID_EMAIL = "test@gmail.com";
+    public static final String VALID_PASSWORD = "Test123!";
     public static final String INVALID_EMAIL = "invalidUser";
     public static final String INVALID_PASSWORD = "invalidPassword";
 }
