31a3437e13db5add2ab6bf376178b2ffbdcdd429
Ivan <ivanalekseenko.post@gmail.com>
2025-04-01 15:44:53 +0200

    Login Webtest

--- src/test/java/wbd/tests/web_tests/LoginTests.java
+++ src/test/java/wbd/tests/web_tests/LoginTests.java
@@ -1,6 +1,30 @@
 package wbd.tests.web_tests;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import wbd.core.TestBaseUI;
+import wbd.web.data.UserData;
+import wbd.web.web_pages.LoginPage;
+
 
 public class LoginTests extends TestBaseUI {
+    @Test
+    public void loginPositiveTest() {
+        // Создаем экземпляр LoginPage
+        LoginPage loginPage = new LoginPage(app.driver, app.wait);
+
+        // click Sign In button
+        app.driver.findElement(By.xpath("//button[contains(text(),'Sign In')]")).click();
+
+        // enter login and password
+        loginPage.enterEmail(UserData.VALID_EMAIL);
+        loginPage.enterPassword("Password!123");
+
+        // click Sign in button
+        app.driver.findElement(By.xpath("//button[contains(text(),'Sign in')]")).click();
+
+        //Проверяем наличие Sign Out
+        Assert.assertTrue(app.driver.findElement(By.xpath("//button[contains(text(),'SignOut')]")).isDisplayed());
+    }
 }
