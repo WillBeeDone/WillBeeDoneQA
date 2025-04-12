40e62465a2a5f59fcb17a91a342b23f95663b5e2
Natalia Tikhomirova <ntikhomirova@gmail.com>
2025-04-12 21:36:47 +0200

    UPD README.md and BasePage

--- src/main/java/wbd/web/data/UserData.java
+++ src/main/java/wbd/web/data/UserData.java
@@ -5,8 +5,8 @@ public class UserData {
     public static final String VALID_PASSWORD = "Password!123";
 
     public static final String BASE_URL = System.getProperty("baseUrl", "https://monkfish-app-73239.ondigitalocean.app");
-    public static final String PASSWORD_VALIDATION_MESSAGE = "Must contain upper & lower case, number, special character. Length 8 or more.";
-            ;
-//    public static final String INVALID_EMAIL = "invalidUser";
-//    public static final String INVALID_PASSWORD = "invalidPassword";
+    public static final String PASSWORD_VALIDATION_MESSAGE =
+            "Must be 8 characters long or more. No white spaces. Must contain at least one: uppercase and lowercase letter, a number, a special character.";
+
+    ;
 }
